47a6bea0abf2fbb8f5f285843d71abd471ba8552
rahul-agam <hidekiryuga0402@gmail.com>
2024-02-24 08:39:19 +0530

    added a log section & lifted up states

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,45 @@
 import { useState } from "react";
 import Player from "./Components/Player.jsx";
 import GameBoard from "./Components/GameBoard.jsx";
+import Log from "./Components/Log.jsx";
 
 function App() {
   // Making player with symbol 'X' as default active player
   // Concept of Lifting State Up (used in both Player & GameBoard components)
   const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare() {
+  // Concept of Lifting State Up (used in both Log & GameBoard components)
+  const [gameTurns, setGameTurns] = useState([]);
+
+  function handleSelectSquare(rowIndex, colIndex) {
     /*
       If user with symbol 'X' is selected, then next
       active player is 'O' and again then 'X'.
-
       So, currentValue depends on Previous value. So, use a function inside the setActivePlayer();
     */
     setActivePlayer((curActivePlayer) => (curActivePlayer === "X" ? "O" : "X"));
-    console.log("Current Active Player inside func : ", activePlayer);
+
+    setGameTurns((prevTurns) => {
+
+      /* Not using the state variable 'activePlayer' here to track the current plyaer, because we are already in a state function for 'gameTurns' , so we cannot gurantee that we get the latest 'activePlayer' here. Because they both are two different state variables. */
+
+      let currentPlayer = 'X';
+
+      // here it is prevTurns[0] - because we should always see the latest player.
+      // if latest player has 'X' then next player should have 'O'
+      if (prevTurns.length > 0 && prevTurns[0].player === 'X') {
+        currentPlayer = 'O';
+      }
+
+      // Since we are dealing with array, we use immutable state update approach.
+      const updatedTurns = [{
+        square: { row: rowIndex, col: colIndex },
+        player: currentPlayer
+      }, ...prevTurns];
+
+      return updatedTurns;
+    })
   }
-  console.log("Current Active Player outside fun : ", activePlayer);
   return (
     <main>
       <div id="game-container">
@@ -35,10 +57,12 @@ function App() {
         </ol>
         <GameBoard
           onSelectSquare={handleSelectSquare}
-          activePlayerSymbol={activePlayer}
+          turns={gameTurns}
         />
       </div>
-      <div id="game-container">LOG</div>
+      <div id="log-container">
+        <Log turns={gameTurns} />
+      </div>
     </main>
   );
 }
--- src/Components/GameBoard.jsx
+++ src/Components/GameBoard.jsx
@@ -6,35 +6,47 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
-  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+export default function GameBoard({ onSelectSquare, turns }) {
 
-  function handleSelectSquare(rowIndex, colIndex) {
-    setGameBoard((prevGameBoard) => {
-      /*
-      Source : https://academind.com/tutorials/reference-vs-primitive-values
+  /*
+  We are managing this state in App.jsx file.
+  ---------------------------------------------------------------------------------------
+    const [gameBoard, setGameBoard] = useState(initialGameBoard);
+    function handleSelectSquare(rowIndex, colIndex) {
+      setGameBoard((prevGameBoard) => {
+  
+      //  Source : https://academind.com/tutorials/reference-vs-primitive-values
+  
+      //  Not a best practice
+      //  -------------------
+      //    Because 'initialGameBoard' is a multi dimensional array, the prevGameBoard input param is just  reference variable, that means any updates on prevGameBoard reference variable , is a direct update on initialGameBoard.    
+      //    That means, even before React schedules & executes this setGameBoard() function, the state//array is already updated. It is not a good practice.
+        -------------------------------
+      //  prevGameBoard[rowIndex][colIndex] = "X";
+      //  return prevGameBoard;
+  
+        //Best Practice
+        const updatedBoard = [
+          ...prevGameBoard.map((innerArray) => [...innerArray]),
+        ];
+        updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
+        return updatedBoard;
+      });
+  
+      // function defined in App.jsx and passed as prop
+      onSelectSquare();
+    }
+  ---------------------------------------------------------------------------------------
+  */
 
-      Not a best practice
-      -------------------
-        Because 'initialGameBoard' is a multi dimensional array, the prevGameBoard input param is just a 
-        reference variable, that means any updates on prevGameBoard reference variable , is a direct update on initialGameBoard. 
-        
-        That means, even before React schedules & executes this setGameBoard() function, the state of array is already updated. It is not a good practice.
-      -------------------------------
-      prevGameBoard[rowIndex][colIndex] = "X";
-      return prevGameBoard;
-      */
+  let gameBoard = initialGameBoard;
 
-      //Best Practice
-      const updatedBoard = [
-        ...prevGameBoard.map((innerArray) => [...innerArray]),
-      ];
-      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
-      return updatedBoard;
-    });
+  for (const turn of turns) {
+    // We get the square selected by the player & his symbol.
+    const { square, player } = turn;
+    const { row, col } = square;
 
-    // function defined in App.jsx and passed as prop
-    onSelectSquare();
+    gameBoard[row][col] = player;
   }
 
   // To print the Game board (9 squares)
@@ -45,7 +57,7 @@ export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => handleSelectSquare(rowIndex, colIndex)}>
+                <button onClick={() => onSelectSquare(rowIndex, colIndex)}>
                   {playerSymbol}
                 </button>
               </li>
